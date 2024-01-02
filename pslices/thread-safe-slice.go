2fe19ed5f583d27d106eb24038c0eb23b244a5f6
Harald Rudell <harald.rudell@gmail.com>
2024-01-02 09:12:15 -0800

    comments

--- pslices/thread-safe-slice.go
+++ pslices/thread-safe-slice.go
@@ -11,19 +11,36 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+// ThreadSafeSlice provides certain thread-safe operations on a slice
+// using [sync.RWMutex] mechanic
+//
+// thread-safety challenges with a slice:
+//   - multiple slice instances may reference the same underlying array
+//   - append operation may change address of data and create multiple arrays
+//   - data race in reading and writing slice elements
+//   - multiple threads accessing a slice instance or their underlying array
+//   - due to slicing creating new memory-sharing slices, the slice value must be
+//     fully enclosed inside the type providing thread-safety
+//   - due to copy and append operations affecting multiple memory addresses,
+//     locking is required for thread-safety
+//   - maintaining authoritative slice-state during iteration
+//
 // All slice operations:
-// - len(slice)
-// - append(slice, E...)
-// - cap(slice)
-// - make([]E, n)
-// - slice[:b]
-// - slice[a:]
-// - slice[a:b]
-// - slice[:]
-// - slice[a]
-// - &slice[a]
-// - &slice
+//   - len(slice)
+//   - append(slice, E...): may create new underlying array
+//   - cap(slice)
+//   - make([]E, n)
+//   - slice[:b]
+//   - slice[a:]
+//   - slice[a:b]
+//   - slice[:]
+//   - slice[a]
+//   - &slice[a]: address of slice element
+//   - &slice: address of slice
 //   - copy(slice, slice2)
+//   - slice literal
+//   - slice variable declaration
+//   - assignment of slice element
 type ThreadSafeSlice[T any] struct {
 	lock  sync.RWMutex
 	slice []T
